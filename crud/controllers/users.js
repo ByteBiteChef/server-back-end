acf17dca3b7d3cdbf8361a3cdbf3c03f55e1aad7
Max Robles <maxirobles1234@gmail.com>
2023-12-15 13:18:37 +0100

    Add project REST API

---
+++ crud/controllers/users.js
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from "uuid"; /// importa la funcion v4 y la renombra a uuidv4. Es para generar ids
+
+let users = []; /// Define un array vacio para almacenar los usuarios
+
+/// exporta la funcion createUser que recive dos argumentos, req(la solicitud) y res(la respuesta). Crea el user
+export const createUser = (req, res) => {
+	const user = req.body; ///extrae el cuerpo de la solicitud y lo almacena en una constante llamada user.
+
+	users.push({ ...user, id: uuidv4() }); /// Añade un nuevo objeto al array users y le agrega un id con la funcion uuidv4.
+	res.send(`User with the name ${user.firstName} added to the database!`); /// envia una respuesta diciendo que el user fue creado
+};
+/// exporta la funcion getUsers. Esta funcion
+export const getUsers = (req, res) => {
+	res.send(users); /// envia una respuesta con el array de users
+};
+/// exporta la funcion getUser
+export const getUser = (req, res) => {
+	const { id } = req.params; /// extrae el parametro id y lo guarda en la constante
+	const foundUser = users.find((user) => user.id == id); /// busca en el array de users un usuario con el mismo id que el de la request y lo almacena
+	res.send(foundUser); /// envia una respuesta con el usuario encontrado
+};
+///exporta la funcion deleteUser
+export const deleteUser = (req, res) => {
+	const { id } = req.params; /// extrae el parametro id y lo guarda en la constante
+	users = users.filter((user) => user.id !== id); /// Filtra el array users y saca el usuario con id sea igual al id extraído de la solicitud. El resultado se almacena de nuevo en la variable users.
+
+	res.send(`User with the id ${id} deleted from the database.`); /// responde que el usuario ha sido eliminado
+};
+/// Exporta una función llamada updateUser
+export const updateUser = (req, res) => {
+	const { id } = req.params; /// extrae el parametro id y lo guarda en la constante
+	const { firstName, lastName, age } = req.body; /// extrae las propiedades firstName, lastName y age y los guarda en un aconstante
+	const user = users.find((user) => user.id == id); ///busca en el array un usuario con el mismo id al de la solicitud
+
+	if (firstName) user.firstName = firstName; /// si se ingresa firstName en la solicitud se reasigna el valor
+	if (lastName) user.lastName = lastName; /// si se ingresa lastName en la solicitud se reasigna el valor
+	if (age) user.age = age; /// si se ingresa age en la solicitud se reasigna el valor
+
+	res.send(`User with the id ${id} has been updated`);/// ernvia una respuesta diciendo que el usuario se actualizo
+};
---
+++ crud/index.js
@@ -0,0 +1,24 @@
+/// importa express que es un framework para node.js
+import express from "express";
+/// importa body-parser module, analiza las request en un middleware
+import bodyParser from "body-parser";
+/// importa modulo userRouter desde user.js del archivo routes. Contine los routes.
+import usersRoutes from "./routes/users.js";
+
+/// This line is creating an instance of an Express application. The `express()` function is a top-level function exported by the Express module.
+const app = express();
+/// Declara el valor del numero de puerto donde el servidor recive las request
+const PORT = 4000;
+
+/// Esta línea dice a la aplicación que utilice el middleware body-parser para analizar las request entrantes en JSON
+app.use(bodyParser.json());
+/// La app usa las rutas definidas en userRoutes
+app.use("/users", usersRoutes);
+/// Define la ruta Get para la ruta ("/"), cuando se accede a la ruta hace el console.log
+app.get("/", (req, res) => {
+	res.send("hello from homepage");
+});
+/// Se inicia el servidor y muestra en la consola que funciona.
+app.listen(PORT, () =>
+	console.log(`Server running on port: http://localhost;${PORT}`)
+);
