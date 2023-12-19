15e41e3a6b74d5b19b1892e1897dfca8cdbf4e8f
Max Robles <maxirobles1234@gmail.com>
2023-12-19 11:21:22 +0100

    Add logic for a Up Date request

--- req.js
+++ req.js
@@ -1,4 +1,5 @@
 document.querySelector("form").addEventListener("submit", handdlePostRequest);
+document.querySelector("form").addEventListener("edit", handdleUpDateRequest);
 
 fetch("http://localhost:4000/users")
 	.then((response) => {
@@ -18,6 +19,28 @@ async function handdlePostRequest(event) {
 
 	fetch("http://localhost:4000/users", {
 		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			firstName,
+			lastName,
+			age,
+		}),
+	})
+		.then((response) => response)
+		.then((data) => console.log(data))
+		.catch((error) => console.log(error));
+}
+
+async function handdleUpDateRequest(event) {
+	event.preventDefault();
+
+	const firstName = document.getElementById("firstName").value;
+	const lastName = document.getElementById("lastName").value;
+	const age = document.getElementById("age").value;
+	const id = document.getElementById("userId").value;
+
+	fetch(`http://localhost:4000/users/${id}`, {
+		method: "PUT",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({
 			firstName,
